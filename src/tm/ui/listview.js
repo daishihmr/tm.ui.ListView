d150d991c49b6baa42957e3bbfc87a962df35909
daishihmr <daishi.hmr@gmail.com>
2015-07-01 13:48:51 +0900

    origin, event

--- src/tm/ui/listview.js
+++ src/tm/ui/listview.js
@@ -16,7 +16,6 @@ tm.define("tm.ui.ListView", {
             children: {
                 innerPanel: {
                     type: "tm.display.CanvasElement",
-                    width: this.width,
                 }
             }
         });
@@ -74,16 +73,23 @@ tm.define("tm.ui.ListView", {
     },
     
     rebuild: function() {
-        this.innerPanel.originX = this.originX;
-        this.innerPanel.originY = this.originY;
-
         this.innerPanel.removeChildren();
+        this.innerPanel.originX = 0.5;
+        this.innerPanel.originY = 0.5;
+        this.innerPanel.width = this.width;
+        this.innerPanel.x = this.width * (0.5 - this.originX);
 
         this.innerPanel.height = 0;
         var y = this.height * -this.originY;
         this.model.forEach(function(item, index) {
             var view = this.createView(item, index).addChildTo(this.innerPanel);
+            this.attach(item, view, index);
+            
+            view.x = view.width * (view.originY - 0.5);
             view.y = y + view.height * view.originY;
+            
+            this.setItemPointingEventListener(view, item, index);
+
             y += view.height;
             this.innerPanel.height += view.height;
         }.bind(this));
@@ -97,6 +103,72 @@ tm.define("tm.ui.ListView", {
         
         return this;
     },
+    
+    refresh: function() {
+        this.innerPanel.children.forEach(function(view, index) {
+            this.attach(this.model[index], view, index);
+        }.bind(this));
+    },
+    
+    setItemPointingEventListener: function(view, item, index) {
+        var self = this;
+        
+        var nomove = false;
+        view.interactive = true;
+        view.on("pointingstart", function(e) {
+            if (!self.interactive) return;
+            if (!self.isHitPoint(e.pointing.x, e.pointing.y)) return;
+            
+            nomove = true;
+        });
+        view.on("pointingmove", function(e) {
+            if (!self.interactive) return;
+            if (!self.isHitPoint(e.pointing.x, e.pointing.y)) return;
+
+            if (e.app.pointing.deltaPosition.length() > 5) {
+                nomove = false;
+            }
+        });
+        view.on("pointingend", function(e) {
+            if (!self.interactive) return;
+            if (!self.isHitPoint(e.pointing.x, e.pointing.y)) return;
+
+            if (nomove) {
+                self.flare("select", {
+                    app: e.app,
+                    view: view,
+                    item: item,
+                    index: index,
+                    model: self.model,
+                    views: self.innerPanel.children,
+                });
+            }
+        });
+    },
+});
+
+tm.define("tm.ui.ListView.DefaultItemView", {
+    superClass: "tm.display.RectangleShape",
+    init: function(text) {
+        this.superInit({
+            width: tm.global.SCREEN_WIDTH || 640,
+            height: 48,
+            fillStyle: "white",
+            strokeStyle: "black",
+            lineWidth: 0.1,
+            boundingType: "rect",
+        });
+        this.fromJSON({
+            children: {
+                label: {
+                    type: "tm.display.Label",
+                    init: [text, 24],
+                    fillStyle: "black",
+                    strokeStyle: "transparent",
+                }
+            }
+        });
+    },
 });
 
 tm.ui.ListView.DEFAULT_PARAM = {
@@ -105,23 +177,9 @@ tm.ui.ListView.DEFAULT_PARAM = {
     fillStyle: "rgb(200, 200, 200)",
     strokeStyle: "transparent",
     createView: function(item, index) {
-        return tm.display.RectangleShape({
-            width: this.width,
-            height: 48,
-            fillStyle: "white",
-            strokeStyle: "black",
-            lineWidth: 0.1,
-        }).fromJSON({
-            children: {
-                label: {
-                    type: "tm.display.Label",
-                    init: ["" + item, 24],
-                    fillStyle: "black",
-                    strokeStyle: "transparent",
-                }
-            }
-        });
+        return tm.ui.ListView.DefaultItemView("" + item);
     },
+    attach: function(item, view, index) {},
 };
 
 })();
